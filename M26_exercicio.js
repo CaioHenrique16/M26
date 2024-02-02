596ac39454c2501da8c9a68cb3cbed12fd747118
Caio Henrique <99083793+CaioHenrique16@users.noreply.github.com>
2024-02-02 20:27:05 -0300

    Add files via upload

---
+++ M26_exercicio.js
@@ -0,0 +1,9 @@
+"use strict";
+//multiplicação
+const multiplicar = (a, b) => a * b;
+const resultadoMultiplicacao = multiplicar(2, 3);
+console.log(resultadoMultiplicacao);
+//saudação
+const saudacao = (nome) => "Olá " + nome;
+const mensagemSaudacao = saudacao("Caio");
+console.log(mensagemSaudacao);
---
+++ M26_exercicio.ts
@@ -0,0 +1,11 @@
+//multiplicação
+const multiplicar = (a: number, b: number): number => a * b;
+
+const resultadoMultiplicacao: number = multiplicar(2, 3);
+console.log(resultadoMultiplicacao); 
+
+//saudação
+const saudacao = (nome: string): string => "Olá " + nome;
+
+const mensagemSaudacao: string = saudacao("Caio");
+console.log(mensagemSaudacao);
